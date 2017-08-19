a4bac2ed50b9531dc7d3b050a6bc675ab80cc792
David Johnson <djohnson.m@gmail.com>
2017-08-19 03:59:11 +0000

    Update nix build

--- default.nix
+++ default.nix
@@ -1,11 +1,19 @@
 { pkgs ? import <nixpkgs> {} }:
 let
   result = pkgs.haskell.packages.ghcjs.callCabal2nix "miso" (pkgs.fetchFromGitHub {
-   sha256 = "0ylsml2v5fq7z51cvq88yq1h3f4px87n3jfgil8d6xjn11r21sfr";
-   rev = "c4e22369551ad6fc6dc567b984cb0c5f68e469a3";
-   owner = "haskell-miso";
-   repo = "miso";
-}) {};
-in pkgs.haskell.packages.ghcjs.callPackage ./hs-2048.nix {
-  miso = result;
-}
+    sha256 = "0l6dscqhniq8my49ycgid3jwxkn7dgv680cjby9qn063kf2k64df";
+    rev = "8475a6bbc0832e730f9556a371f8131a8b421230";
+    owner = "haskell-miso";
+    repo = "miso";
+  }) {};
+  hs2048 = pkgs.haskell.packages.ghcjs.callPackage ./hs-2048.nix {
+    miso = result;
+  };
+  inherit (pkgs) sass closurecompiler;
+in
+  pkgs.runCommand "hs2048" { inherit hs2048; } ''
+    mkdir -p $out
+    ${sass}/bin/sass ${hs2048.src}/static/main.scss $out/main.css
+    ${closurecompiler}/bin/closure-compiler ${hs2048}/bin/app.jsexe/all.js > $out/all.js
+    cp ${hs2048.src}/static/index.html $out/index.html
+  ''
